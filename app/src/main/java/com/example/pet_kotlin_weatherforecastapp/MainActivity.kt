6e204084e2baa146c1b49447e7cbca95f3288c5e
Stanislav.Koloncev <stas.koloncev232@gmail.com>
2025-05-24 16:29:33 +0200

    https://github.com/Katana1936/PET_Kotlin_WeatherForecastApp.git

---
+++ app/src/main/java/com/example/pet_kotlin_weatherforecastapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.pet_kotlin_weatherforecastapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pet_kotlin_weatherforecastapp.ui.theme.PET_Kotlin_WeatherForecastAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PET_Kotlin_WeatherForecastAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PET_Kotlin_WeatherForecastAppTheme {
+        Greeting("Android")
+    }
+}
